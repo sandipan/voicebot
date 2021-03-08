37a253354dc4f81693eed1c231372a2498672c87
rndpoc <rndpoctcs@gmail.com>
2021-03-09 03:11:30 +0530

    adding / updating intent-handlers and utterances

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 """
 AWS lambda code for Voicebot
-### author: Sandipan Dey
+### authors: Akash Gupta, Sandipan Dey
 """
 
 import random
@@ -9,6 +9,8 @@ import json
 #!pip install pandas
 
 import pandas as pd
+import datetime
+from dateutil.relativedelta import relativedelta
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -39,20 +41,67 @@ def build_response(session_attributes, speechlet_response):
         'response': speechlet_response
     }
 
+def get_a_random_recommendation():
+    plans = ['Health Maintenance Organization (HMO)', 'Preferred Provider Organization (PPO)', 'Point of Service (POS)', 'High Deductible Health (HDHP)',
+        'Short term Health Insurance', 'Gap Insurance']
+    return 'You can go with a {} medical health insurance plan'.format(random.choice(plans))
 
 # --------------- Functions that control the skill's behavior ------------------
-def get_benefit_response(intent, session):
-    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
-    in your alexa skill in order for it to work.
-    """
-    session_attributes = session.get('attributes', {})
-    card_title = "Benefits"
-    speech_output = 'what service do you want to know more about?'
-    reprompt_text = "I said," + speech_output
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+def get_policy_expiry_alert(uid):
+    dfp =  pd.read_csv('prod.csv')
+    dfup = pd.read_csv('userprod.csv')
+    df = pd.merge(dfp, dfup, left_on=['id'], right_on=['pid'], how='inner').drop('id', axis=1)
+    df = df.loc[df.uid == uid]
+    speech_output = ''
+    if len(df) > 0:
+        df['edate'] = pd.to_datetime(df['date'], errors='coerce') + pd.to_timedelta(df.term_yrs*365, unit='D')
+        df['etime'] = df.edate - pd.Timestamp.now().normalize()
+        for index, row in df.iterrows():
+            et = row['etime'].days
+            #print(et)
+            if et < 0:
+                speech_output += 'Your porduct with ID {} got already expired {} days back. '.format(row['pid'], abs(et))
+            elif et < 180: # expiring in next 6 months
+                speech_output += 'Your porduct with ID {} will expire in next {} days. '.format(row['pid'], et)
+    #df.head()
+    return speech_output
 
+#Premium helper function for both ask_premium and alert_premium   
+def get_premium_info(uid, pid):
+
+    user_df = pd.read_csv("user.csv")
+    userprod_df = pd.read_csv("userprod.csv")
+    prod_df = pd.read_csv("prod.csv")
+    userprod_df['date'] = pd.to_datetime(userprod_df['date'])
+    
+    prem_count = userprod_df.loc[(userprod_df.uid == int(uid)) & (userprod_df.pid == int(pid)),'nprpaid'].values[0]
+    prem_amt = prod_df.loc[prod_df.id == int(pid), 'pramt'].values[0]
+    user_name = user_df.loc[user_df.id == int(uid), 'name'].values[0]                           
+    pr_once = prod_df.loc[prod_df.id == int(pid), 'pronce'].values[0]
+    start_date = userprod_df.loc[(userprod_df.uid == int(uid)) & (userprod_df.pid == int(pid)),'date'].values[0]
+    npr_paid = userprod_df.loc[(userprod_df.uid == int(uid)) & (userprod_df.pid == int(pid)),'nprpaid'].values[0]
+
+    return prem_count, prem_amt, user_name, pr_once, npr_paid, start_date
+
+
+def get_premium_alert(uid):
+    
+    df = pd.read_csv("userprod.csv")
+    row = df.loc[(df.uid == id),'pid']
+    speech_output = ''
+    for pid in row.values:
+        prem_count, prem_amt, user_name, pr_once, npr_paid, start_date = get_premium_info(uid, pid)
+        start_date = pd.Timestamp(start_date)
+        today_date = datetime.datetime.now()
+        prem_to_paid = (today_date - start_date).days/365   
+        default_n = int(prem_to_paid) - npr_paid
+        if default_n >= 2:
+            speech_output += 'Hey {} '.format(user_name) + 'It seems that you have missed your last few premiums! please pay it before {}'.format(due_date.strftime("%d")) + 'of {} '.format(due_date.strftime("%B")) + 'in {} '.format(due_date.strftime("%Y"))  
+        else :
+            speech_output += 'Hey {} '.format(user_name) +'No premium due'
+    return speech_output
+
+        
 def get_id_response(intent, session):
     """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
     in your alexa skill in order for it to work.
@@ -61,20 +110,42 @@ def get_id_response(intent, session):
     print(session)
     card_title = "Identify"
     df = pd.read_csv('user.csv')
+    session_attributes = {}
     
-    id = intent['slots']['id']['value']
-    print(id)
-    session_attributes = {'id': int(id)}
-
-    row = df.loc[df.id == int(id), 'name']
-    
-    if len(row) != 0:
-        speech_output = 'Welcome {}. '.format(row.values[0])
+    if 'value' in intent['slots']['id']:
+        id = intent['slots']['id']['value']
+        id = int(''.join(filter(str.isalnum, id)))
+        print(id)
+        session_attributes = {'id': id}
+        row = df.loc[df.id == int(id), 'name']
+        if len(row) != 0:
+            speech_output = 'Welcome {}. '.format(row.values[0])
+        else:
+            speech_output = 'Your id is not there in my database! '
+        alert = get_policy_expiry_alert(id)
+        if alert:
+            speech_output += 'Here are policy expiry alerts for you! ' + alert
+        #alert = get_premium_alert(id)
+        #if alert:
+        #    speech_output += 'Here are premium due alerts for you! ' + alert
+        speech_output += ' Now, tell me what do you want to know about?'
     else:
-        speech_output = 'Your id is not there in my database! '
-    speech_output += 'What do you want to know about?'
+        speech_output = 'Please register to the system first and buy an insurance product! '
+        speech_output += get_a_random_recommendation()
     reprompt_text = "I said " + speech_output
         
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+def get_benefit_response(intent, session):
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = session.get('attributes', {})
+    card_title = "Benefits"
+    speech_output = 'what service do you want to know more about?'
+    reprompt_text = "I said," + speech_output
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -112,15 +183,83 @@ def get_service_response(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
         
+def get_premium_response(intent, session):
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    print("SESSION BELOW")
+    print(session)
+    session_attributes = session.get('attributes', {})
+    card_title = "Premium"
+    df = pd.read_csv('userprod.csv')
+    id = int(session_attributes['id'])
+    row = df.loc[(df.uid == id),'pid']
+    speech_output = ''
+    if len(row) > 0:
+        speech_output = 'You have the following product(s) with ID(s): ' + ', '.join(map(str,row.values)) + '. '
+        speech_output += 'Which product do you want to know your premium information about?'
+    else:
+        speech_output = 'You have not bought any products yet! Buy a product first.'
+    reprompt_text = "I said," + speech_output
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_product_response(intent, session):
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    card_title = "Product"
+    session_attributes = session.get('attributes', {})
+    uid = int(session_attributes['id'])
+    pid = intent['slots']['pid']['value']
+    pid = int(''.join(filter(str.isalnum, pid)))
+    prem_count, prem_amt, user_name, pr_once, npr_paid, start_date = get_premium_info(uid, pid)
+    start_date = pd.Timestamp(start_date)
+    due_date = start_date + relativedelta(years =1)
+    today_date = datetime.datetime.now()
+    if pr_once == 'yes':
+        speech_output = 'Hey {} '.format(user_name) + 'your total premium amount is {} '.format(prem_amt) + 'Which is due before {} '.format(due_date.strftime("%d")) + 'of {} '.format(due_date.strftime("%B")) + 'in {} '.format(due_date.strftime("%Y")) 
+    else :
+        due_date = start_date + relativedelta((today_date - start_date).days/365)
+        speech_output = 'Hey {} '.format(user_name) + 'your total premium amount is {} '.format(prem_amt) +  "and you have paid {} ".format(npr_paid) + 'premiums till now! And, your next due date is on {} '.format(due_date.strftime("%d")) + 'of {} '.format(due_date.strftime("%B")) + 'in {} '.format(due_date.strftime("%Y")) 
+  
+    reprompt_text = "I said " + speech_output
+        
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
 def get_recommend_response():
     """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
     in your alexa skill in order for it to work.
     """
     session_attributes = {}
     card_title = "Recommendation"
-    plans = ['Health Maintenance Organization (HMO)', 'Preferred Provider Organization (PPO)', 'Point of Service (POS)', 'High Deductible Health (HDHP)',
-        'Short term Health Insurance', 'Gap Insurance']
-    speech_output = 'You can go with a {} medical health insurance plan'.format(random.choice(plans))
+    speech_output = get_a_random_recommendation()
+    reprompt_text = "I said," + speech_output
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+def get_unregister_response(intent, session):
+    
+    session_attributes = session.get('attributes', {})
+    card_title = "Unregister"
+    uid = int(session_attributes['id'])
+    df = pd.read_csv("user.csv")
+    df.drop(df[df.id == uid].index, inplace=True)
+    #df.to_csv('user.csv')
+    print(df)
+    df = pd.read_csv("userprod.csv")
+    df.drop(df[df.uid == uid].index, inplace=True)
+    #df.to_csv('userprod.csv')
+    print(df)
+    df = pd.read_csv("userclaim.csv")
+    df.drop(df[df.uid == uid].index, inplace=True)
+    #df.to_csv('userclaim.csv')
+    print(df)
+    speech_output = 'Removed your ID {} from memory! Sorry, Can\'t change the readonly database! Bye'.format(uid)
     reprompt_text = "I said," + speech_output
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
@@ -133,12 +272,21 @@ def get_welcome_response():
     """
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Hello, I am your recommendation assitant, your application has started! you can ask for your benefits or ask for help! First tell me your user id!"
+    speech_output = "Hello, I am your recommendation assitant, your application has started! you can ask for your benefits, premiums, recommendations, remove your ID or ask for help! First tell me your user id!"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "I don't know if you heard me, I am your recommendation assitant! Tell me your user id!"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def handle_fallback_intent():
+    card_title = "Fallback Intent"
+    speech_output = "Sorry, I could not understand what you said! " \
+                    "Can you say that again?"
+    reprompt_text = 'I said, ' + speech_output
+    should_end_session = False
+    return build_response({}, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 
@@ -184,12 +332,20 @@ def on_intent(intent_request, session):
         return get_benefit_response(intent, session)
     elif intent_name == "askservice":
         return get_service_response(intent, session)
+    elif intent_name == "askpremium":
+        return get_premium_response(intent, session)
+    elif intent_name == "askproduct":
+        return get_product_response(intent, session)
     elif intent_name == "askrecommend":
         return get_recommend_response()
+    elif intent_name == "removeid":
+        return get_unregister_response(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
+    elif intent_name == "AMAZON.FallbackIntent":
+        return handle_fallback_intent()
     else:
         raise ValueError("Invalid intent")
 
